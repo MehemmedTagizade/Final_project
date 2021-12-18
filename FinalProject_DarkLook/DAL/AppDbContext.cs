a41b18b4a1b3226276c5c73791063e4f8f35e7c0
Your Name <you@example.com>
2021-12-18 15:41:40 +0400

    a

---
+++ FinalProject_DarkLook/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FinalProject_DarkLook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject_DarkLook.DAL
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ FinalProject_DarkLook/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject_DarkLook.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [Required, StringLength(100)]
+
+        public string Name { get; set; }
+
+        [Required, StringLength(100)]
+
+        public string SurName { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
