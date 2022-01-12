9ce992a71ca3b58e494be85fba52370d234de0c0
Your Name <you@example.com>
2022-01-12 19:55:15 +0400

    adminTry

--- FinalProject_DarkLook/Areas/Manage/Controllers/DashboardController.cs
+++ FinalProject_DarkLook/Areas/Manage/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,11 @@ using System.Threading.Tasks;
 namespace FinalProject_DarkLook.Areas.Manage.Controllers
 {
         [Area("manage")]
+    [Authorize(Roles = "Admin")]
+
     public class DashboardController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
